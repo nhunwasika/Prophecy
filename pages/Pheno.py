8ca0c170a715f8fd63f3ff062a7da4316901b39e
nhunwasika <129248207+nhunwasika@users.noreply.github.com>
2023-10-03 16:07:46 +0700

    Update Pheno.py

--- pages/Pheno.py
+++ pages/Pheno.py
@@ -9,7 +9,7 @@ from sklearn.metrics import r2_score,mean_squared_error
 st.title("Index Price Prediction")
 st.header("Index Price Prediction from NPRU")
 
-df=pd.read_csv('./data/stock_index_price.csv')
+df=pd.read_csv('./Data/stock_index_price.csv')
 st.write(df.head(10))
 
 #st.line_chart(df)
